e3401d441aff1c3c0ab1e9720a9d81b957474eeb
Jack Carroll <jackcarroll2021@yahoo.com>
2023-04-11 13:14:32 -0500

    Homepage styling better

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,9 @@
 export const Header = () => {
     return (
         <>
-        <div className="bg-light">
+        <div className="bg-light fixed-top" >
             <div className="d-flex justify-content-between py-1">
-                <h5 className="d-inline-block m-1 text-success p-1 mx-1">Workout App</h5>
+                <h5 className="d-inline-block m-1 p-1 mx-3">Workout App</h5>
                 <div className="d-inline-block mx-4">
                     <a className="d-inline-block mx-5 " style={{textDecoration: 'none', color: '#003F7D', fontWeight: 'bold'}}>Login</a>
                     <a className="d-inline-block my-1 py-2 px-4 text-white rounded" style={{backgroundColor: 'orange', textDecoration: 'none', fontWeight:"bold"    }}>SIGN UP</a>
--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,13 +1,21 @@
 import { Header } from "./Header";
 
 export const Homepage = () => {
-    return (
-        <>
-            <Header />
-            <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-                <h1 className="text-center">The Best Workout App Ever Created</h1>
-                <p className="text-center">Created in part by: Ashley Cardot, Chandler Casteel, Giovanni Boscan, Jack Carroll, Joshua Ayodele, Will Epperly</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-primary position-relative">
+        <img
+          src="https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt="Workout"
+          className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover"
+        />
+        <div className="position-absolute top-50 start-50 translate-middle text-center text-white">
+          <h1 className="text-center">The Best Workout App Ever Created</h1>
+          <p className="text-center">Create your perfect workout today.</p>
+        </div>
+      </div>
+    </>
+  );
 };
