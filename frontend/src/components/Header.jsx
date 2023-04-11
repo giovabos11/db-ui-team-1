3e57a342a0d22dc8618712bdc22a7b85b9f0b9ce
Jack Carroll <jackcarroll2021@yahoo.com>
2023-04-10 19:43:07 -0500

    Added header comp for later

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
 export const Header = () => {
-    return <>
-        <div>
-            <h3 className="text-left">Workout App</h3>
-
-
-
-        </div>
-    </>
-}
+    return (
+        <>
+            <div className="d-flex justify-content-between m-2">
+                <h4 className="d-inline-block">Workout App</h4>
+                <div className="d-inline-block">
+                    <h4 className="d-inline-block" >Sign in</h4>
+                    <h4 className="d-inline-block">Log in</h4>
+                </div>
+            </div>
+        </>
+    );
+};
