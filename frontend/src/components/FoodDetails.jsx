69a5dbaf9c24424563ff827a3b4f3986509d4de5
Jack Carroll <jackcarroll2021@yahoo.com>
2023-04-13 01:51:08 -0500

    Food Details looks better

--- frontend/src/components/FoodDetails.jsx
+++ frontend/src/components/FoodDetails.jsx
@@ -1,34 +1,39 @@
 export const FoodDetails = () => {
     return <>
-    
-    <div className="container border rounded p-5">
-        <div className=" p-5">
-            <h3>Today's Macronutrient recap:</h3>
-            <input type="text-area" />
 
-            
-            <hr />
-            <h3>Log a new food</h3>
+        <div className="container border rounded p-3">
+            <div className=" p-3">
+                <div className="p-3">
+                    <h3>Today's Macronutrient recap:</h3>
+                    <input type="text-area" />
+                </div>
 
-            <h4>Food Name</h4>
-            <input type="text"/>
+                <hr />
+                <h3>Log a new food</h3>
+                <div className="p-3">
+                    <h4>Food Name</h4>
+                    <input type="text" />
+                </div>
 
-            <h4>Protein Amount (grams)</h4>
-            <input type="text"/>
+                <div className="p-3">
+                    <h4>Protein Amount (grams)</h4>
+                    <input type="text" />
+                </div>
+                <div className="p-3">
+                    <h4>Carbohydrate Amount (grams)</h4>
+                    <input type="text" />
+                </div>
+                <div className="p-3">
+                    <h4>Fat Amount (grams)</h4>
+                    <input type="text" />
+                </div>
 
-            <h4>Carbohydrate Amount (grams)</h4>
-            <input type="text"/>
 
-            <h4>Fat Amount (grams)</h4>
-            <input type="text"/>
 
-            
 
-            
-            
+            </div>
+            <button className="m-2 btn btn-primary">Submit Food</button>
         </div>
-        <button className="m-2 btn btn-primary">Submit Food</button>
-        </div>
-    
+
     </>
 }
