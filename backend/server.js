07bf1a88dbda35c438cf20fdf9aa4eefa5f20988
WillEpperly <106688887+WillEpperly@users.noreply.github.com>
2023-04-17 21:54:40 -0500

    added profile router

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import { profileRouter } from "./routes/profileRouter";
+
 const express = require("express");
 const app = express();
 const port = 8000;
@@ -9,76 +11,80 @@ app.use(cors());
 
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "NeverSkipLegDay",
-    database: "DBUI",
+  host: "localhost",
+  user: "root",
+  password: "NeverSkipLegDay",
+  database: "DBUI",
 });
 
 connection.connect();
 
+app.use("/profile", profileRouter);
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.put("/parse", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    try {
-        const body = req.body;
-        const name = body["first"] + " " + body["last"];
-        const age = body["age"];
-        const isAdmin = body["admin"] ? "is an admin" : "is not an admin";
-        const message = `${name} is ${age} years old and ${isAdmin}`;
-        res.status(200);
-        res.send(message);
-    } catch {
-        console.log(err);
-    }
+  try {
+    const body = req.body;
+    const name = body["first"] + " " + body["last"];
+    const age = body["age"];
+    const isAdmin = body["admin"] ? "is an admin" : "is not an admin";
+    const message = `${name} is ${age} years old and ${isAdmin}`;
+    res.status(200);
+    res.send(message);
+  } catch {
+    console.log(err);
+  }
 });
 
 app.get("/db", (req, res) => {
-    connection.query("SHOW TABLES", (err, rows, fields) => {
-        if (err) throw err;
+  connection.query("SHOW TABLES", (err, rows, fields) => {
+    if (err) throw err;
 
-        console.log(rows);
-        res.status(200);
-        res.send(rows);
-    });
+    console.log(rows);
+    res.status(200);
+    res.send(rows);
+  });
 });
 
 app.post("/user", (req, res) => {
-    const { first, last, age, admin } = req.body;
-    const query = `INSERT INTO users (first_name, last_name, age, admin) VALUES ('${first}', '${last}', ${age}, ${admin})`;
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err;
+  const { first, last, age, admin } = req.body;
+  const query = `INSERT INTO users (first_name, last_name, age, admin) VALUES ('${first}', '${last}', ${age}, ${admin})`;
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err;
 
-        console.log(rows);
-        res.status(200);
-        res.send("Successfully added user!");
-    });
+    console.log(rows);
+    res.status(200);
+    res.send("Successfully added user!");
+  });
 });
 
 app.post("/users", (req, res) => {
-    const query = `SELECT * FROM users;`;
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err;
+  const query = `SELECT * FROM users;`;
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err;
 
-        res.status(200);
-        res.send(rows);
-    });
+    res.status(200);
+    res.send(rows);
+  });
 });
 
 app.post("/users/clear", (req, res) => {
-    const query = `DELETE FROM users;`;
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err;
+  const query = `DELETE FROM users;`;
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err;
 
-        res.status(200);
-        res.send("Successfully cleared users!");
-    });
+    res.status(200);
+    res.send("Successfully cleared users!");
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post("/profile/workouts", (req, res) => {});
