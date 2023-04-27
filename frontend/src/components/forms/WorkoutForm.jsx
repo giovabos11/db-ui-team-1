178f358b3024e0848a884b1d352447bb86129348
Jack Carroll <jackcarroll2021@yahoo.com>
2023-04-26 20:06:21 -0500

    Workout form styled

--- frontend/src/components/forms/WorkoutForm.jsx
+++ frontend/src/components/forms/WorkoutForm.jsx
@@ -8,7 +8,7 @@ import { AppContext } from "../global/AppContext"
 import { add_workouts_from_exercises } from "../../api/allApi"
 
 
-export const WorkoutForm = ({ showPopup, setShowPopup,cartItems, setCartItems }) => {
+export const WorkoutForm = ({ showPopup, setShowPopup, cartItems, setCartItems }) => {
     const appContext = useContext(AppContext);
     const [description, setDescription] = useState("");
     const [muscle, setMuscle] = useState("");
@@ -33,86 +33,90 @@ export const WorkoutForm = ({ showPopup, setShowPopup,cartItems, setCartItems })
 
         if (cartItems.length !== 0) {
             let exercisesIds = [];
-      
+
             cartItems.forEach(item => {
-              exercisesIds.push(item.id);
+                exercisesIds.push(item.id);
             });
-      
+
             add_workouts_from_exercises(appContext.type, appContext.id, exercisesIds, muscle, exercisesIds.length * 10, weekday, description);
             setCartItems([]);
-          }
-          setDescription("");
-          setMuscle("");
-          setWeekday("");
-          setShowPopup(false);
+        }
+        setDescription("");
+        setMuscle("");
+        setWeekday("");
+        setShowPopup(false);
     }
 
     return <>
-    <div className="">
-    {showPopup && (
-        <>
-        <form onSubmit={handleSubmit} className="bg-dark d-flex flex-column position-fixed start-50 translate-middle-x p-3 " style={{width:"30%", zIndex:9999, top:"25%"}}>
-        <label>
-            <select className="form-select-lg" id="weekday" value={weekday} onChange={handleWeekday}>
-                <option value="">Choose a weekday</option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-                <option value="Saturday">Saturday</option>
-                <option value="Sunday">Sunday</option>
-            </select>
-        </label>
-        <label>
-            <div className="">
-                <input
-                    type="text"
-                    id="muscleGroup"
-                    name="muscleGroup"
-                    value={muscle}
-                    onChange={handleMuscle}
-                    placeholder="Enter a muscle group"
-                    className="form-input-sm"
-                />
-            </div>
-        </label>
-        <label>
-            <div className="">
-                <textarea
-                    id="description"
-                    name="description"
-                    value={description}
-                    onChange={handleDescription}
-                    placeholder="Enter a description"
-                    className="form-control" style={{ }}
-                    rows="4"
-                ></textarea>
-            </div>
-        </label>
+        <div>
+            {showPopup && (
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <div className="d-flex flex-column position-fixed start-50 translate-middle-x p-3 rounded" style={{ width: "30%", zIndex: 9999, top: "25%", backgroundColor: "#F5F5DC" }}>
+                            <label>Day</label>
+                                <select className="form-select-sm" id="weekday" value={weekday} onChange={handleWeekday}>
+                                    <option value="">Choose a weekday</option>
+                                    <option value="Monday">Monday</option>
+                                    <option value="Tuesday">Tuesday</option>
+                                    <option value="Wednesday">Wednesday</option>
+                                    <option value="Thursday">Thursday</option>
+                                    <option value="Friday">Friday</option>
+                                    <option value="Saturday">Saturday</option>
+                                    <option value="Sunday">Sunday</option>
+                                </select>
+                            
+                            <label>Muscle</label>
+                                <div className="">
+                                    <input
+                                        type="text"
+                                        id="muscleGroup"
+                                        name="muscleGroup"
+                                        value={muscle}
+                                        onChange={handleMuscle}
+                                        placeholder="Enter a muscle group"
+                                        className="form-control"
+                                    />
+                                </div>
+                            
+                            <label>Description</label>
+                                <div className="">
+                                    <textarea
+                                        id="description"
+                                        name="description"
+                                        value={description}
+                                        onChange={handleDescription}
+                                        placeholder="Enter a description"
+                                        className="form-control" 
+                                        style={{ height: "", resize: "none" }}
+                                        rows="4"
+                                    ></textarea>
+                                </div>
+                            
 
-        
-        <label  className="" style={{ height: '' }}>
-            <div className="form-group">
-                <Button type="submit " className="btn btn-primary" >
-                    Submit
-                </Button>
-            </div>
-        </label>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <div className="m-3">
+                                    <label>         
+                                            <Button type="submit " className="btn btn-primary" >
+                                                Submit
+                                            </Button>
+                                    </label>
+                                </div>
+                                
+                                <div className="m-3">
+                                    <label>
+                                        <Button className="btn btn-danger"
+                                            onClick={() => {
+                                                setShowPopup(false)
+                                            }}
+                                        >Cancel</Button>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </>
+            )}
 
-        <label>
-            <Button
-            onClick={() =>{
-                setShowPopup(false)
-            }}
-            >Cancel</Button>
-        </label>
-    </form>
-
-
+        </div>
     </>
-
-    )}
-    </div>
-    </>       
 }
